5c690f24bebd17940ed8f07c6a93de0d4496c5a5
tokuyama <hitomaruland@gmail.com>
2025-07-22 07:11:21 +0900

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://ametrec/github.io",
+	site: "https://ametrec.github.io",
 	base: "/ametrec2.0",
 	trailingSlash: "always",
 	integrations: [
