bca3c8ddf76c969b734dcf97a3303c9a593d2071
tokuyama <hitomaruland@gmail.com>
2025-07-22 08:48:02 +0900

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -27,7 +27,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 // https://astro.build/config
 export default defineConfig({
 	site: "https://ame-trec.github.io",
-	base: "/ame-trec2.0",
+	base: "/ame-trec2.0/",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
